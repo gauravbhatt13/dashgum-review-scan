d90ab50a051129c74135d4ce4f1618d59bb9270c
gauravbhatt13 <gauravbhatt13@gmail.com>
2019-04-20 23:49:48 +0530

    fixed amazon changes

--- routes/amazonScan.js
+++ routes/amazonScan.js
@@ -28,57 +28,63 @@ var init = function () {
     reviews = [];
 };
 
-var callback = function ($, res, pageCount) {
-    if (pageCount == 0 || callbackCounter++ < pageCount) {
+var callback = function ($, res, baseUrl) {
+    var elem = $('div[data-reftag="cm_cr_arp_d_paging_btm"]');
+    var callUrl = $(elem).children('ul').children('li').eq(1).children('a').attr('href');
+    if (callbackCounter == 5 || callUrl === undefined) {
+        var replacedCharsTitle = aggregateReviews.replace(/[^a-zA-Z0-9 ]/g, "");
+        var splitTitle = replacedCharsTitle.split(' ');
+        var stopWordsRemovedArray = sw.removeStopwords(splitTitle);
+
+        var ngram = new Ngram({
+            n: 2
+        });
+
+        var bigram = ngram.ngram(stopWordsRemovedArray.join(' '));
+        var combined = [];
+        bigram.forEach(function (item) {
+            combined.push(item.join(' '));
+        });
+
+        var ngramCounts = _.countBy(combined, function (word) {
+            return word;
+        });
+
+        responseData.imageSrc = $('a[class="a-link-normal"]').children().eq(0).attr('src');
+        responseData.prodName = $('a[data-hook="product-link"]').eq(0).text();
+        responseData.totalReviewCount = $('span[data-hook="total-review-count"]').text();
+        responseData.starRatings = $('div[class="a-row averageStarRatingNumerical"]').children('span').text();
+        responseData.wordDict = ngramCounts;
+        responseData.reviews = reviews;
+        res.send(responseData);
+    } else {
         $("[id^=customer_review-]").each(function (i, element) {
             createReviews($, element);
         });
-        if (callbackCounter == pageCount) {
-            var replacedCharsTitle = aggregateReviews.replace(/[^a-zA-Z0-9 ]/g, "");
-            var splitTitle = replacedCharsTitle.split(' ');
-            var stopWordsRemovedArray = sw.removeStopwords(splitTitle);
-
-            var ngram = new Ngram({
-                n: 2
-            });
-
-            var bigram = ngram.ngram(stopWordsRemovedArray.join(' '));
-            var combined = [];
-            bigram.forEach(function (item) {
-                combined.push(item.join(' '));
-            });
-
-            var ngramCounts = _.countBy(combined, function (word) {
-                return word;
-            });
-
-            responseData.imageSrc = $('a[class="a-link-normal"]').children().eq(0).attr('src');
-            responseData.prodName = $('a[data-hook="product-link"]').eq(0).text();
-            responseData.totalReviewCount = $('span[data-hook="total-review-count"]').text();
-            responseData.starRatings = $('div[class="a-row averageStarRatingNumerical"]').children('span').text();
-            responseData.wordDict = ngramCounts;
-            responseData.reviews = reviews;
-            res.send(responseData);
-        }
+        request(baseUrl + callUrl, {gzip: true}, function (error, response, html) {
+            callbackCounter++;
+            $ = cheerio.load(html);
+            callback($, res, baseUrl);
+        });
     }
 };
 var handleFewReviews = function (req, res) {
     var baseUrl = '';
     console.log(req.body.url);
-    if(req.body.url.includes('amazon.in')){
+    if (req.body.url.includes('amazon.in')) {
         baseUrl = 'https://www.amazon.in';
-    }else{
+    } else {
         baseUrl = 'https://www.amazon.com';
     }
 
-    request(req.body.url, function (error, response, html) {
+    request(req.body.url, {gzip: true}, function (error, response, html) {
         if (!error && response.statusCode == 200) {
             var $ = cheerio.load(html);
-            if($('a[data-hook="see-all-reviews-link"]').attr('href') != undefined){
+            if ($('a[data-hook="see-all-reviews-link"]').attr('href') != undefined) {
 
                 var allReviews = baseUrl + $('a[id="acrCustomerReviewLink"]').attr('href');
                 triggerSearch(baseUrl, allReviews, res, callback);
-            }else{
+            } else {
                 res.send(responseData);
             }
         }
@@ -88,11 +94,48 @@ var handleFewReviews = function (req, res) {
 var triggerSearch = function (baseUrl, url, res, callback) {
     console.log('called service: ' + url);
     var dataRef = [];
-    request(url, function (error, response, html) {
+    request(url, {gzip: true}, function (error, response, html) {
         try {
             $ = cheerio.load(html);
-            var pageCount = $('li[data-reftag="cm_cr_arp_d_paging_btm"]').length;
-            if (pageCount == 0) {
+            var elem = $('div[data-reftag="cm_cr_arp_d_paging_btm"]');
+            var callUrl = $(elem).children('ul').children('li').eq(1).children('a').attr('href');
+            if (callUrl) {
+                request(baseUrl + callUrl, {gzip: true}, function (error, response, html) {
+                    $ = cheerio.load(html);
+                    callback($, res, baseUrl);
+                });
+            } else {
+                $("[id^=customer_review-]").each(function (i, element) {
+                    createReviews($, element);
+                });
+                var replacedCharsTitle = aggregateReviews.replace(/[^a-zA-Z0-9 ]/g, "");
+                var splitTitle = replacedCharsTitle.split(' ');
+                var stopWordsRemovedArray = sw.removeStopwords(splitTitle);
+
+                var ngram = new Ngram({
+                    n: 2
+                });
+
+                var bigram = ngram.ngram(stopWordsRemovedArray.join(' '));
+                var combined = [];
+                bigram.forEach(function (item) {
+                    combined.push(item.join(' '));
+                });
+
+                var ngramCounts = _.countBy(combined, function (word) {
+                    return word;
+                });
+
+                responseData.imageSrc = $('a[class="a-link-normal"]').children().eq(0).attr('src');
+                responseData.prodName = $('a[data-hook="product-link"]').eq(0).text();
+                responseData.totalReviewCount = $('span[data-hook="total-review-count"]').text();
+                responseData.starRatings = $('div[class="a-row averageStarRatingNumerical"]').children('span').text();
+                responseData.wordDict = ngramCounts;
+                responseData.reviews = reviews;
+                res.send(responseData);
+            }
+            //var pageCount = $('li[data-reftag="cm_cr_arp_d_paging_btm"]').length;
+            /*if (pageCount == 0) {
                 callback($, res, pageCount);
             } else {
                 $('li[data-reftag="cm_cr_arp_d_paging_btm"]').each(function (i, element) {
@@ -102,7 +145,7 @@ var triggerSearch = function (baseUrl, url, res, callback) {
                         callback($, res, pageCount);
                     });
                 });
-            }
+            }*/
         } catch (error) {
             console.log(error);
         }
@@ -111,9 +154,9 @@ var triggerSearch = function (baseUrl, url, res, callback) {
 
 var createReviews = function ($, element) {
     var review = {};
-    review.title = $(element).children().eq(0).children('a[data-hook="review-title"]').text();
+    review.title = $(element).children().eq(1).children('a[data-hook="review-title"]').text();
     //review.date = $(element).children().eq(1).children('span[data-hook="review-date"]').text();
-    review.comment = $(element).children().eq(3).children('span[data-hook="review-body"]').text();
+    review.comment = $(element).children().eq(4).children('span[data-hook="review-body"]').text();
     var sentimentAnalysis = sentiment.analyze(review.comment);
     review.score = sentimentAnalysis.score;
     reviews.push(review);
